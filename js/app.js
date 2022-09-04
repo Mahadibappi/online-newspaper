149169182842966b5df8020d3463276337d8c50b
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-09-04 20:17:20 +0600

    news dynamic added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+const loadCategory = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategory(data.data.news_category);
+};
 
+const displayCategory = (category) => {
+  const container = document.getElementById("category");
+  category.forEach((element) => {
+    const divCategory = document.createElement("div");
+    divCategory.classList.add("col");
+    divCategory.innerHTML = `
+    <ul class="d-flex justify-content-around fs-5">
+        <li class="list-group-item border-0">${element.category_name}</li>
+        
+    </ul>
+           
+      `;
+    container.appendChild(divCategory);
+  });
+};
+loadCategory();
